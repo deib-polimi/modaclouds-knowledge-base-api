702da81f345e70717306df4e1ea226490e998d7b
Marco Miglierina <marco.miglierina@icloud.com>
2014-08-24 20:48:31 +0200

    licence

--- src/main/java/it/polimi/modaclouds/monitoring/kb/api/KBConfig.java
+++ src/main/java/it/polimi/modaclouds/monitoring/kb/api/KBConfig.java
@@ -1,3 +1,19 @@
+/**
+ * Copyright 2014 deib-polimi
+ * Contact: deib-polimi <marco.miglierina@polimi.it>
+ *
+ *    Licensed under the Apache License, Version 2.0 (the "License");
+ *    you may not use this file except in compliance with the License.
+ *    You may obtain a copy of the License at
+ *
+ *        http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *    Unless required by applicable law or agreed to in writing, software
+ *    distributed under the License is distributed on an "AS IS" BASIS,
+ *    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *    See the License for the specific language governing permissions and
+ *    limitations under the License.
+ */
 package it.polimi.modaclouds.monitoring.kb.api;
 
 import com.hp.hpl.jena.rdf.model.Property;
@@ -8,6 +24,7 @@ import com.hp.hpl.jena.rdf.model.impl.ResourceImpl;
 
 public class KBConfig {
 
+	// TODO namespace and uriPrefix are both here and in qos-models
 	public static final String namespace = "http://www.modaclouds.eu/rdfs/1.0/entities#";
 	public static final String uriPrefix = "modaent";
 	
